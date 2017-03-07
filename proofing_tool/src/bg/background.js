4966f48da03aea2712c74ce6dfea99f2dac76860
ivorpad <ivorjpc@gmail.com>
2017-03-06 23:05:22 -0400

    Cleaned up a couple of variables and lines

--- proofing_tool/src/bg/background.js
+++ proofing_tool/src/bg/background.js
@@ -1,18 +1,15 @@
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.type == "notification")
-      chrome.notifications.create('notification', request.options, function() {
-        // callback
-      });
+      chrome.notifications.create('notification', request.options, function() { });
       
     chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
-      var activeNotifications = {};
       
-            if (btnIdx === 0) {
-                chrome.tabs.update(sender.tab.id, {active: true}, function() {});
-            } else if (btnIdx === 1) {
-                chrome.notifications.clear(notifId, function() {});
-            }
+      if (btnIdx === 0) {
+          chrome.tabs.update(sender.tab.id, {active: true}, function() {});
+      } else if (btnIdx === 1) {
+          chrome.notifications.clear(notifId, function() {});
+      }
       
     });
 
